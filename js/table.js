fb97c6d4a68fd0da6adeae1e4f408fc781ee5d9d
Elnur Kurtaliev <el92@yandex.ru>
2015-08-02 11:43:23 +0300

    First stable scroll

---
+++ js/table.js
@@ -0,0 +1,180 @@
+/**
+ * Created by EL on 30.07.2015.
+ */
+
+var Table = {
+    data: [],
+    part: 0,
+    show: {
+        limit: 100,
+        rowHeight: 30,
+        position: 0,
+        height:null,
+        buffer:0,
+        header: {
+            container: null,
+            columns: null
+        },
+        data: {
+            container: null,
+            before: null,
+            table: null,
+            after: null
+        }
+    },
+    schema: [
+        {
+        name: 'gender',
+        sortable: false,
+        format: false
+    }, {
+        name: 'first_name',
+        sortable: true,
+        format: false
+    }, {
+        name: 'email',
+        sortable: true,
+        format: 'hyperlink'
+    }, {
+        name: 'country',
+        sortable: true,
+        format: false
+    }, {
+        name: 'ip_address',
+        sortable: false,
+        format: false
+    }, {
+        name: 'married',
+        sortable: false,
+        format: 'boolean'
+    }, {
+        name: 'debet',
+        sortable: true,
+        format: 'number'
+    }, {
+        name: 'geo-latitude',
+        sortable: false,
+        format: false
+    }, {
+        name: 'geo-longitude',
+        sortable: false,
+        format: false
+    }, {
+        name: 'number',
+        sortable: true,
+        format: 'roundnumber'
+    }],
+    view: null,
+    loadData: function () {
+        jQuery.ajax({
+            url: 'data/MOCK_DATA(' + this.part + ').json',
+            method: 'GET',
+            success: function (data) {
+                Table.data = Table.data.concat(data);
+                Table._renderData();
+                Table.part++;
+            }
+        });
+    },
+    _renderHeader: function () {
+        this.show.header.columns = document.createElement('table');
+        var row = this.show.header.columns.appendChild(document.createElement('tr'));
+        var width = 100 / this.schema.length;
+        this.schema.forEach(function (column) {
+            var cell = document.createElement('th');
+            if (column.sortable) {
+                cell.className = 'sortable';
+            }
+            cell.style.width = width + '%';
+            row.appendChild(cell);
+            cell = cell.appendChild(document.createElement('span'));
+            cell.innerHTML = column.name;
+        });
+        this.show.header.container.appendChild(this.show.header.columns);
+    },
+    _renderData: function (direction) {
+        //var t = new Date();
+        var downState = this.show.position;
+        if (direction === 'up') {
+            this.show.position = this.show.buffer - this.show.limit + (this.show.height * 2);
+            this.show.position = this.show.position < 0 ? 0 : this.show.position;
+            if (downState === this.show.position) {
+                return;
+            }
+        } else if (direction === 'down') {
+            this.show.position =
+                this.show.buffer > this.data.length - this.show.limit ?
+                    this.data.length - this.show.limit :
+                    this.show.buffer - this.show.height;
+            if (downState === this.show.position) {
+                return;
+            }
+        }
+
+
+
+        this.show.data.container.style.height = this.data.length * this.show.rowHeight + 'px';
+        this.show.header.container.style.width = this.show.data.container.clientWidth + 'px';
+        this.show.data.before.style.height = this.show.position * this.show.rowHeight + 'px';
+        this.show.data.after.style.height = (this.data.length - this.show.position - this.show.limit) * this.show.rowHeight + 'px';
+        this.show.data.table.innerHTML = '';
+        var rows = document.createElement('tbody');
+        var width = Math.floor(this.show.data.container.clientWidth / this.schema.length) -1 ;
+        for (var i = this.show.position; i < this.show.position + this.show.limit; i++) {
+            if (i>=Table.data.length) {
+                console.log('alert', i); return;
+            }
+            var row = document.createElement('tr');
+            this.schema.forEach(function (value) {
+                var item = document.createElement('td');
+                //item.style.width =  + '%';
+
+                var data = Table.data[i][value.name];
+                if (/^(M|F)$/i.test(data)) {
+                    data = i;
+                }
+                item.innerHTML = '<span class="cell-data" style="width:' + width + 'px;">' + data + '</span>';
+                row.appendChild(item);
+            });
+            rows.appendChild(row);
+        }
+        this.show.data.table.appendChild(rows);
+    },
+    viewScroll: function () {
+        Table.show.buffer = Math.abs(Math.ceil($(Table.show.data.container).position().top / Table.show.rowHeight));
+        //console.log(Table.show.buffer);
+        var range = {
+            top: Table.show.position,
+            bottom: Table.show.position + Table.show.limit
+        };
+        var visRange = {
+            top: Table.show.buffer - Table.show.height >= 0 ? Table.show.buffer - Table.show.height : 0,
+            bottom: Table.show.buffer + (Table.show.height * 2)
+        };
+        //console.log('scroll', range.top, range.bottom, visRange.top, visRange.bottom);
+        var direction =
+            visRange.bottom > range.bottom ?
+                'down' :
+                visRange.top < range.top ?
+                    'up' :
+                    false;
+        if (direction) {
+            Table._renderData(direction);
+            //console.log('loading', direction, range.top, range.bottom, visRange.top, visRange.bottom);
+            console.log('loading', direction);
+        }
+    }
+    ,
+    init: function (containerId) {
+        this.view = document.getElementById(containerId);
+        this.show.header.container = this.view.querySelector('.header');
+        this.show.data.container = this.view.querySelector('.container');
+        this.show.height = Math.floor(this.view.querySelector('.data').clientHeight / this.show.rowHeight);
+        $('.data').on('scroll', this.viewScroll);
+        this.show.data.before = this.show.data.container.querySelector('.before');
+        this.show.data.table = this.show.data.container.querySelector('.data-table');
+        this.show.data.after = this.show.data.container.querySelector('.after');
+        this._renderHeader();
+        this.loadData();
+    }
+};
